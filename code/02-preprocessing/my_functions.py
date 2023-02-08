ba9c3a9e60e6b140460b9ec688b273adf27afc90
Arturo <soberonarturo@gmail.com>
2023-02-08 09:48:35 -0600

    Added basic try/exc structure and began logging

--- code/02-preprocessing/my_functions.py
+++ code/02-preprocessing/my_functions.py
@@ -1,22 +1,38 @@
 # Import pandas
+import logging
 import pandas as pd
+
+# Log config
+logging.basicConfig(
+    filename='../logs/my_functions.log',
+    filemode='w',
+    datefmt='%Y-%m-%d %H:%M:%S',
+    level=logging.INFO
+)
 
 # Fill all missing values with either mean or mode
 def fill_all_missing_values(data):
-
-    # Iterate over columns
-    for col in data.columns:
-
-        # Fill numeric columns with mean
-        if data[col].dtype in ['float64', 'int64']:
-            data[col].fillna(
-                data[col].mean(),
-                inplace=True
-            )
+    try:
         
-        # Fill all other types with mode
-        else:
-             data[col].fillna(
-                data[col].mode().item(),
-                inplace=True
-            )
+        # Assert type
+        assert(type(data) == pd.core.frame.DataFrame)
+
+        # Iterate over columns
+        for col in data.columns:
+
+            # Fill numeric columns with mean
+            if data[col].dtype in ['float64', 'int64']:
+                data[col].fillna(
+                    data[col].mean(),
+                    inplace=True
+                )
+            
+            # Fill all other types with mode
+            else:
+                data[col].fillna(
+                    data[col].mode().item(),
+                    inplace=True
+                )
+    except:
+        
+        True
